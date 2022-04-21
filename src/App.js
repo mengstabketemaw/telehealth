3e01587d2db9d9cbcac7611b7cbfdcbf235050ef
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-04-21 03:21:52 -0700

    Router path has been added[initial]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Login from "./pages/Login";
+import {Routes,Route,Navigate} from "react-router"
 
 function App() {
   return (
-    <>
-    <Login/>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="login" replace/>}/>    
+      <Route path="login" element={<Login/>}/>
+    </Routes>
     );
 }
 
