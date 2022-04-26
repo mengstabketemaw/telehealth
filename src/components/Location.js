a3e4b15967bdb9e211bcfc6621a04a229168e7cd
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-04-26 13:15:03 -0700

    Create-account has been modified and location file has been added

---
+++ src/components/Location.js
@@ -0,0 +1,182 @@
+import {Wrapper,Status} from "@googlemaps/react-wrapper"
+import React, { useEffect, useRef,useState } from "react"
+import {createCustomEqual} from "fast-equals";
+import {Button, Grid,Container, Stack} from "@mui/material"
+import Geocode from "react-geocode"
+
+Geocode.setApiKey("AIzaSyDqEI8VirEe1RwV7uqz_nTp9VpQAORVjT8");
+Geocode.setRegion("et");
+Geocode.setLocationType("ROOFTOP")
+
+
+const status = (s)=>{
+    <h1>{s}</h1>
+}
+
+const Location = ({setUserLocation,setModalOpen}) =>{
+    const [clicks, setClicks] = useState([]);
+    const [zoom, setZoom] = useState(15);
+    const [center,setCenter] = useState({lat:9,lng:38});
+    const onClick = (e)=>{
+        setClicks([e?.latLng]);
+      }
+
+    const setMarkerUserLocation = (m) => {
+        if (window.navigator.geolocation) {
+            window.navigator.geolocation.getCurrentPosition(
+              (position) => {
+                setClicks([new window.google.maps.LatLng(position.coords.latitude,position.coords.longitude)])
+                setCenter({lat:position.coords.latitude,lng:position.coords.longitude})
+                setZoom(zoom);
+                //coverting location coordinate to street name and country name should be coded here 
+                // Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
+                //     (response) => {
+                //       const address = response.results[0].formatted_address;
+                //       console.log(address);
+                //     },
+                //     (error) => {
+                //       console.error(error);
+                //     }
+                //   );
+              },
+              () => {
+                console.log("error in navigator position")
+            }
+            );
+          } else {
+            // Browser doesn't support Geolocation
+            console.log("browser doesn't support geolocation")
+        }
+    }
+
+
+    const onIdle =(m)=>{
+      setMarkerUserLocation(m);
+    }
+
+    return <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+            >
+                <Stack direction={"row"} justifyContent={"center"} alignItems={"center"} spacing={3}>
+                    <Button variant="contained" color="secondary" onClick={()=>setMarkerUserLocation()}>Mark My Location</Button>
+                    <Button variant="contained" color="primary"
+                      onClick={()=>{
+                        setModalOpen(false);
+                        setUserLocation({lat:clicks[0].lat(),lng:clicks[0].lng()})
+                      }
+                    }>Choose Location</Button>
+                </Stack>
+                <Wrapper apiKey="AIzaSyDqEI8VirEe1RwV7uqz_nTp9VpQAORVjT8" render={status}>
+                    <Map 
+                    center={center}
+                    onClick={onClick}
+                    onIdle={onIdle}
+                    zoom={zoom}
+                    style={{height:"400px",width:"100%"}}
+                    >
+                        {clicks.map((latLng, i) => (<Marker key={i} position={latLng} /> ))}
+                    </Map>
+                </Wrapper>
+        </Stack>
+}
+
+
+const Map =({onClick, onIdle, children, style,...options})=>{
+    const ref = useRef(null);
+    const [map,setMap] = useState();
+    
+    useEffect(()=>{
+        if(ref?.current&&!map){
+            setMap(new window.google.maps.Map(ref.current,{}))
+        }
+    },[ref,map]);
+
+    useDeepCompareEffectForMaps(() => {
+        if (map) {
+          map.setOptions(options);
+        }
+      }, [map, options]);
+
+    useEffect(() => {
+        if (map) {
+          ["click", "idle"].forEach((eventName) =>
+            window.google.maps.event.clearListeners(map, eventName)
+          );
+          if (onClick) {
+            map.addListener("click", onClick);
+          }
+      
+          if (onIdle) {
+            map.addListener("idle", () => onIdle(map));
+          }
+        }
+      }, [map, onClick, onIdle]);
+      
+   
+
+    return <>
+    <div ref={ref} style={style} />
+    {React.Children.map(children, (child) => {
+      if (React.isValidElement(child)) {
+        // set the map prop on the child component
+        return React.cloneElement(child, { map });
+      }
+    })}
+  </>
+}
+
+const Marker = (options) => {
+    const [marker, setMarker] = useState();
+  
+    React.useEffect(() => {
+      if (!marker) {
+        setMarker(new window.google.maps.Marker());
+      }
+  
+      // remove marker from map on unmount
+      return () => {
+        if (marker) {
+          marker.setMap(null);
+        }
+      };
+    }, [marker]);
+  
+    React.useEffect(() => {
+      if (marker) {
+        marker.setOptions(options);
+      }
+    }, [marker, options]);
+  
+    return null;
+  };
+  
+
+const deepCompareEqualsForMaps = createCustomEqual(
+    (deepEqual) => (a, b) => {
+      if (a instanceof window.google.maps.LatLng || b instanceof window.google.maps.LatLng ) {
+        return new window.google.maps.LatLng(a).equals(new window.google.maps.LatLng(b));
+      }
+      return deepEqual(a, b);
+    }
+  );
+
+function useDeepCompareMemoize(value) {
+    const ref =useRef();
+  
+    if (!deepCompareEqualsForMaps(value, ref.current)) {
+      ref.current = value;
+    }
+  
+    return ref.current;
+  }
+
+function useDeepCompareEffectForMaps(callback, dependencies) {
+    useEffect(callback, dependencies.map(useDeepCompareMemoize));
+}
+  
+
+
+export default Location;
--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,45 +1,183 @@
-import { Grid, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
+import { Button, Grid,Box,FormControl,InputLabel, MenuItem, Modal, Select, Stack, TextField, Typography, Input, IconButton, FormControlLabel, Checkbox } from "@mui/material";
 import { useState } from "react";
+import Location from "../components/Location"
+import {AdapterLuxon} from "@mui/x-date-pickers/AdapterLuxon"
+import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { ContactSupportOutlined, PhotoCamera } from "@mui/icons-material";
 
 function CreateAccount() {
-    const [user,setUser]=useState("patient");
+    const [user, setUser] = useState("patient");
+    const [modalOpen,setModalOpen] = useState(false);
+    const [userLocation,setUserLocation] = useState({lat:0,lng:0});
+    const [userInfo, setUserInfo] = useState({});
 
-
-    return ( <Grid container height={"100vh"} direction={"column"} justifyContent={"flex-start"} alignItems={"center"} >
-                <Grid item xs>
-                    <Stack direction={"row"} spacing={3}>
-                        <Typography variant="h2" color={"primary"}>Register</Typography>
-                        <Select
-                        value={user}
-                        onChange={(e)=>setUser(e.target.value)}
-                        style={{borderStyle:"none"}}
-                        variant="standard"
-                        renderValue={(value)=>(<Typography variant="p" color="primary">As a {value}</Typography>)}
-                        >
-                            <MenuItem value="patient">patient</MenuItem>
-                            <MenuItem value="doctor">doctor</MenuItem>
-
-                        </Select>
-                    </Stack>
-                </Grid>
-                <Grid item xs>
-                    <Stack direction={"row"} justifyContent={"space-between"}>
-                        <TextField
-                         label={"First Name"}
-                        />
-                        <TextField
-                         label={"First Name"}
-                        />
-                        <TextField
-                         label={"First Name"}
-                        />
-                         </Stack>
-                </Grid>
-
-
-
+    const renderBasedOnUser = () =>{
+        if(user==="patient"){
+            return<Grid item xs={8}>
+            <FormControl sx={{ m: 1, minWidth: "100%" }}>
+                <InputLabel>Martial Status</InputLabel>
+             <Select
+                label="Martial Status"
+                value={userInfo?.martialStatus||''}
+                onChange={e=>{setUserInfo({...userInfo,martialStatus:e.target.value})}}
+            >
+                <MenuItem value={"merride"}>Merride</MenuItem>
+                <MenuItem value={"unmerride"}>UnMerride</MenuItem>
+            </Select>
+            </FormControl>
+        </Grid>
+        }
+        return<> <Grid item xs={4}>
+            <FormControl  sx={{ m: 1, minWidth: "100%" }}>
+                <InputLabel>Role</InputLabel>
+                <Select
+                    label="Role"
+                    multiple
+                    value={userInfo?.doctorRole||[]}
+                    onChange={e=>{
+                        let value = e.target.value;
+                        let userValue =  typeof value === 'string' ? value.split(',') : value;
+                        setUserInfo({...userInfo,doctorRole:userValue});
+                                }
+                            }
+                >
+                    <MenuItem value={"consultation"}>Consultation</MenuItem>
+                    <MenuItem value={"vdt"}>VDT</MenuItem>
+                    <MenuItem value={"therapist"}>Therapist</MenuItem>
+                </Select>
+            </FormControl> 
             </Grid>
-     );
+            <Grid item xs={4}>
+            <TextField
+                fullWidth
+                focused
+                label="Qualification/Specialization"
+                type="file"
+            />
+            </Grid>
+        </>
+    }
+
+
+    return (<>
+    <Grid padding={4} container direction={"row"} justifyContent={"flex-start"} alignItems={"center"} spacing={3}>
+        <Grid item xs={12}>
+            <Stack direction={"row"} spacing={3} >
+                <Typography variant="h2" color={"primary"}>Register</Typography>
+                <Select
+                    value={user}
+                    onChange={(e) => setUser(e.target.value)}
+                    variant="standard"
+                    renderValue={(value) => (<Typography variant="p" color="primary">As a {value}</Typography>)}
+                >
+                    <MenuItem value="patient">patient</MenuItem>
+                    <MenuItem value="doctor">doctor</MenuItem>
+                </Select>
+            </Stack>
+        </Grid>
+        <Grid item xs={4}>
+            <TextField fullWidth label={"First Name"}/>
+        </Grid>
+        <Grid item xs={4}>
+            <TextField fullWidth label={"MIddle Name"}/>
+        </Grid>
+        <Grid item xs={4}>
+            <TextField fullWidth label={"Last Name"}/>
+        </Grid>
+        <Grid item xs={4}>
+            <TextField fullWidth aria-readonly value = {userLocation.lat} label={"Latitude"}/>
+        </Grid>
+        <Grid item xs={4}>
+            <TextField fullWidth aria-readonly value={userLocation.lng} label={"Longtiude"}/>
+        </Grid>
+        <Grid item xs={4}>
+            <Button onClick={()=>setModalOpen(true)}>Choose Location</Button>
+        </Grid>
+        <Grid item xs={6}>
+            <TextField fullWidth label={"Phone Number"}/>
+        </Grid>
+        <Grid item xs={6}>
+            <TextField fullWidth label={"Home Phone Number"}/>
+        </Grid>
+        <Grid item xs={4}>
+            <TextField 
+              fullWidth 
+              label={"Email"} 
+              type="email"
+              helperText={"used to login to the system"}
+            />
+        </Grid>
+        <Grid item xs={4}>
+            <TextField 
+              fullWidth 
+              label={"Password"}
+              type="password"
+              helperText=" "
+            />
+        </Grid>
+        <Grid item xs={4}>
+            <TextField 
+              fullWidth 
+              label={"Confirm Password"}
+              type="password"
+              helperText=" "
+            />
+        </Grid>
+        <Grid item xs={2}>
+            <LocalizationProvider dateAdapter={AdapterLuxon}>    
+                <DatePicker 
+                    label="Birth Date"
+                    value={userInfo?.birthDate||null}
+                    onChange={value=>{setUserInfo({...userInfo,birthDate:value})}}
+                    renderInput={(parms)=><TextField {...parms}/>}
+                />
+            </LocalizationProvider>
+        </Grid>
+        <Grid item xs={2}>
+            <FormControl sx={{m:1,minWidth:130}}>
+                <InputLabel>Gender</InputLabel>
+                <Select
+                label="Gender"
+                value={userInfo?.gender||""}
+                onChange={(e)=>{setUserInfo({...userInfo,gender:e.target.value})}}
+                >
+                    <MenuItem value="male">Male</MenuItem>
+                    <MenuItem value="female">Female</MenuItem>
+                </Select>
+            </FormControl>
+        </Grid>
+        {
+            renderBasedOnUser()
+        }
+        <Grid item xs={3}>
+            <Stack spacing={3}>
+                <Stack direction={"row"} spacing={3}>
+                    <Typography>Profile Picture</Typography>
+                    <label>
+                        <IconButton color="primary" component="span">
+                            <PhotoCamera/>
+                        </IconButton>
+                        <Input  style={{display:"none"}} id="icon-button-file" accept="image/*" type="file"/>
+                    </label>
+                </Stack>
+                <Stack direction={"row"}>
+                    <FormControlLabel sx={{margin:0,padding:0.1}} control={<Checkbox/>} label="I agreee to "></FormControlLabel>
+                    <Button>Term and Condition</Button>
+                </Stack>
+                <Button variant="contained">Register</Button>
+                <Button>Already have an account</Button>
+            </Stack>
+        </Grid>
+    </Grid>
+    <Modal
+      open={modalOpen}
+      onClose={()=>setModalOpen(false)}
+     >
+        <Box sx = {{position:"absolute",top:"20%",width:"100%"}}>
+            <Location setUserLocation={setUserLocation} setModalOpen={setModalOpen}/>
+        </Box>
+    </Modal>
+    </>);
 }
 
 export default CreateAccount;
