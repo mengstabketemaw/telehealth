8c47c0c4bb445db3a210a063530faeb4abfd68e3
mengstab <mengstab@gmail.com>
2022-05-04 21:07:27 -0700

    another version

--- src/pages/patient/Appintment.js
+++ src/pages/patient/Appintment.js
@@ -1,9 +1,61 @@
+import {DataGrid,useGridApiRef,useGridApiContext} from "@mui/x-data-grid"
+import { Button } from "@mui/material"
+
+const row = [
+    {id:1,name:"mrx"},
+    {id:2,name:"mry"},
+    {id:3,name:"mrz"},
+]
+
+const column = [
+    {
+        field:"name",
+        editable:true
+    }
+]
+
+
+const AddToolbar = (props) => {
+    const apiRef = useGridApiContext();
+    
+    const handleClick=()=>{
+        const id = 12;
+        console.log(apiRef)
+        apiRef.current.updateRows([{ id, isNew: true }]);
+        apiRef.current.startRowEditMode({ id });
+        setTimeout(() => {
+            apiRef.current.setCellFocus(id, 'name');
+          });
+    }
+
+
+    return <Button onClick = {handleClick} onMouseDown={e=>e.preventDefault()}>
+        add new record
+    </Button>
+}
+
+
 const Appointment = ()=>{
-    return (<>
-    
-    <h1>This is Appointment</h1>
-    
+    const apiRef = useGridApiRef();
+
+    return <>
+    <div style={{height:"400px",width:'100%'}}>
+        <DataGrid
+            rows={row}
+            columns={column}
+            apiRef={apiRef}
+            editMode="row"
+            experimentalFeatures={{newEditingApi:true}}
+            components={{
+                Toolbar:AddToolbar
+            }}
+            componentsProps={{
+                toolbar:{
+                    apiRef
+                }
+            }}
+        />
+    </div>    
     </>
-    )
 }
 export default Appointment;
