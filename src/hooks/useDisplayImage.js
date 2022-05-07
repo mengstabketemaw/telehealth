83d499a46c6d0426cca76512b65e5ce22f34dd25
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-05-07 13:54:23 -0700

    profile page has been configured 50% from the ui prespective

---
+++ src/hooks/useDisplayImage.js
@@ -0,0 +1,21 @@
+
+//from https://stackoverflow.com/questions/43992427/how-to-display-a-image-selected-from-input-type-file-in-reactjs
+
+import { useState } from "react";
+
+export function useDisplayImage() {
+    const [result, setResult] = useState("");
+
+    function uploader(e) {
+      const imageFile = e.target.files[0];
+
+      const reader = new FileReader();
+      reader.addEventListener("load", (e) => {
+        setResult(e.target.result);
+      });
+
+      reader.readAsDataURL(imageFile);
+    }
+
+    return { result, uploader };
+  }
--- src/pages/patient/Profile.js
+++ src/pages/patient/Profile.js
@@ -1,8 +1,79 @@
+import { Photo, PhotoCamera } from "@mui/icons-material";
+import { Avatar, Box, Button, Container, Dialog, DialogContent, DialogTitle, IconButton, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { useDisplayImage } from "../../hooks/useDisplayImage";
+
+
 const Profile = ()=>{
+    const [profile,setProfile] = useState({});
+    const {result,uploader} = useDisplayImage();
+
     return (<>
-    
-    <h1>This is Profile</h1>
-    
+    <br/>
+    <Typography variant="h4" color="primary">Profile</Typography>
+    <br/>
+    <Box sx={{width:"100%"}}>
+        <Container sx={{width:"fit-content"}}>
+            <Stack alignItems={"center"} spacing={0}>
+                <Avatar sx={{width:"150px",height:"150px"}}
+                    src={result}
+                >A</Avatar>
+                <label>
+                    <input 
+                        style={{display:"none"}} id="icon-button-file" type="file" accept="image/*"
+                        files={[profile?.image||""]}  
+                        onChange={e=>{
+                            uploader(e);
+                            setProfile({...profile,image:URL.createObjectURL(e.target.files[0])})
+                        }}
+                        />
+                    <IconButton component="span" color="primary">
+                        <PhotoCamera/>
+                    </IconButton>
+                </label>
+            </Stack>
+        </Container>
+        <Box sx={{marginBottom:"50px",display:"flex",alignItems:"flex-start", spacing:"10",flexWrap:"wrap",justifyContent:"space-evenly"}}>
+        <TextField
+            label={"First name"}
+        />
+        <TextField
+            label={"Middle name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        
+        <TextField
+            label={"First name"}
+        />
+        <TextField
+            label={"Middle name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        <TextField
+            label={"Last name"}
+        />
+        </Box>
+        <Button variant="contained">Save</Button>
+    </Box>
     </>
     )
 }
