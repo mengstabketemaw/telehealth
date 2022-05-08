cfb3d55e0bb6701427dcbd17c346da4891640553
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-05-08 08:54:13 -0700

    ui 50% finished

--- src/pages/doctor/Office.js
+++ src/pages/doctor/Office.js
@@ -1,6 +1,19 @@
+import { Box, Typography } from "@mui/material";
+import { border } from "@mui/system";
+
 const Office = () => {
     return (<>
-    <h1>This is Office</h1>
+    <br/>
+    <Typography variant="h4" color="primary">Wellcome to Your Virtual Office Sir.</Typography>
+    <br/>
+    <Box sx={{width:"100%",height:"70vh",display:"flex",flexWrap:"wrap", border:"1px solid green"}}>
+        <Box sx={{width:"60%",height:"100%",borderRight:"1px solid green"}}>
+
+        </Box>
+        <Box sx={{width:"auto", height:"100%",border:"1px solid green"}}>
+
+        </Box>
+    </Box>
     </>)
 }
 export default Office;
