a29c206c9262d7292e3f2126f91fa9e713652a67
Mengstab Ketemaw <mengstabrealk28@gmail.com>
2022-05-27 20:43:53 -0700

    initial file

--- src/pages/doctor/DocIndex.js
+++ src/pages/doctor/DocIndex.js
@@ -1,23 +1,23 @@
 import { useState } from "react";
-import {ShowLocations} from "../../components/maps/MapUtils"
+import Location from "../../components/maps/MapUtils"
 
 const users = [
-  {name:"mamush",position:[9,38]},
-  {name:"mereed",position:[9.12,38.44]},
-  {name:"mamaru",position:[9.4,38.2]},
-  {name:"matiwos",position:[9.32,38.90]},
+  { name: "mamush", position: [9, 38] },
+  { name: "mereed", position: [9.12, 38.44] },
+  { name: "mamaru", position: [9.4, 38.2] },
+  { name: "matiwos", position: [9.32, 38.90] },
 ]
 
 const DocIndex = () => {
-const [position,setPosition] = useState();
-const setLocation = (lat,lng) =>{
-  setPosition({lat,lng});
-}
-return <>
-  
-  <p>Hello Doctor how r u{position?.lat}-{position?.lng}</p>
-  <ShowLocations users={users}/>
-</>
-    
+  const [position, setPosition] = useState();
+  const setLocation = (lat, lng) => {
+    setPosition({ lat, lng });
+  }
+  return <>
+
+    <p>Hello Doctor how r u{position?.lat}-{position?.lng}</p>
+    <Location setLocation={setLocation} />
+  </>
+
 }
 export default DocIndex;
