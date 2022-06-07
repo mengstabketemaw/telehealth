b5e6298154199c4d61d489035394c38f0791d70c
mengstabketemaw <mengstabrealk28@gmail.com>
2022-06-07 12:54:51 +0300

    add zIndex property to the top bar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,83 +1,87 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import { styled, alpha } from '@mui/material/styles'
-import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import { Avatar,Tooltip,Box, Menu, MenuItem, Divider } from '@mui/material';
+import * as React from "react"
+import AppBar from "@mui/material/AppBar"
+import { styled, alpha } from "@mui/material/styles"
+import SearchIcon from "@mui/icons-material/Search"
+import InputBase from "@mui/material/InputBase"
+import IconButton from "@mui/material/IconButton"
+import MenuIcon from "@mui/icons-material/Menu"
+import Toolbar from "@mui/material/Toolbar"
+import { Avatar, Tooltip, Box, Menu, MenuItem, Divider } from "@mui/material"
 import useToken from "../hooks/useToken"
-import Config from '../api/Config';
-import { useNavigate } from 'react-router-dom';
-import Auth from '../api/Auth';
+import Config from "../api/Config"
+import { useNavigate } from "react-router-dom"
+import Auth from "../api/Auth"
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.black,0.035),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.black,0.05),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  }));
-  
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-  
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('md')]: {
-        width: '20ch',
-      },
-    },
-  }));
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.035),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.05),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}))
 
-const Topbar = ({handleDrawerToggle}) =>{
-  const {token,setToken} = useToken();
-  const nav = useNavigate();
-  const [auth] = React.useState(new Auth(token,setToken));
-  const [anchorEl, setAnchorEl] = React.useState(null);
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}))
+
+const Topbar = ({ handleDrawerToggle }) => {
+  const { token, setToken } = useToken()
+  const nav = useNavigate()
+  const [auth] = React.useState(new Auth(token, setToken))
+  const [anchorEl, setAnchorEl] = React.useState(null)
   const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-  };
-    
-  const handleClose = () => {
-        nav("profile")
-        setAnchorEl(null);
-  };
+    setAnchorEl(event.currentTarget)
+  }
 
-    const handleLogout = () => {
-      setAnchorEl(null);
-      auth.logoutUser(()=>{nav("/")});
-    }
-    return (<>
-     <AppBar
+  const handleClose = () => {
+    nav("profile")
+    setAnchorEl(null)
+  }
+
+  const handleLogout = () => {
+    setAnchorEl(null)
+    auth.logoutUser(() => {
+      nav("/")
+    })
+  }
+  return (
+    <>
+      <AppBar
         position="fixed"
         elevation={0}
         color="transparent"
         sx={{
+          zIndex: 1000,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
           ml: { sm: `${drawerWidth}px` },
         }}
@@ -88,7 +92,7 @@ const Topbar = ({handleDrawerToggle}) =>{
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
@@ -98,42 +102,40 @@ const Topbar = ({handleDrawerToggle}) =>{
             </SearchIconWrapper>
             <StyledInputBase
               placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </Search>
           <Box sx={{ flexGrow: 1 }} />
           <Tooltip title={"Open Setting"}>
-              <IconButton onClick={handleMenu}>
-                  <Avatar src={`${Config.USER_URL}/avatar/${token.username}`}/>
-              </IconButton>
+            <IconButton onClick={handleMenu}>
+              <Avatar src={`${Config.USER_URL}/avatar/${token.username}`} />
+            </IconButton>
           </Tooltip>
           <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={()=>setAnchorEl(null)}
-            >
-              <MenuItem disabled>{token.username}</MenuItem>
-              <MenuItem disabled>{token.role}S Account</MenuItem>
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <Divider/>
-              <MenuItem onClick={handleLogout}>Logout</MenuItem>
-            </Menu>
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={() => setAnchorEl(null)}
+          >
+            <MenuItem disabled>{token.username}</MenuItem>
+            <MenuItem disabled>{token.role}S Account</MenuItem>
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <Divider />
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
-      
-    
-    
-    </>)
+    </>
+  )
 }
-export default Topbar;
+export default Topbar
