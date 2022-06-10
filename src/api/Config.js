c756cbfc97929b1d421b786c1e100ec8c56baf40
mengstabketemaw <mengstabrealk28@gmail.com>
2022-06-10 17:10:40 +0300

    change the base to heroku

--- src/api/Config.js
+++ src/api/Config.js
@@ -1,86 +1,87 @@
 class Config {
-  SCHEME = process.env.SCHEME ? process.env.SCHEME : "http";
-  HOST = process.env.HOST ? process.env.HOST : "localhost";
-  PORT = process.env.PORT ? process.env.PORT : "8080";
-  BASE = `${this.SCHEME}://${this.HOST}:${this.PORT}`;
+  SCHEME = process.env.SCHEME ? process.env.SCHEME : "http"
+  HOST = process.env.HOST ? process.env.HOST : "localhost"
+  PORT = process.env.PORT ? process.env.PORT : "8080"
+  BASE = "https://telehealth-oauth-server.herokuapp.com" //`${this.SCHEME}://${this.HOST}:${this.PORT}`;
   VIDEOSERVER = `${this.SCHEME}://${this.HOST}:8081`
-  USER_URL = `${this.BASE}/api/user`;
-  AUTH_URL = `${this.BASE}/api/oauth`;
-  ADMIN_URL = `${this.BASE}/api/admin`;
+  USER_URL = `${this.BASE}/api/user`
+  AUTH_URL = `${this.BASE}/api/oauth`
+  ADMIN_URL = `${this.BASE}/api/admin`
 
-
-  ACCESS_TOKEN = "accessToken";
-  EXPIRATION = "expiration";
+  ACCESS_TOKEN = "accessToken"
+  EXPIRATION = "expiration"
 
   defaultHeaders() {
     return {
       "Content-Type": "application/json",
-    };
+    }
   }
 
   headersWithAuthorization() {
     return {
       ...this.defaultHeaders(),
       Authorization: localStorage.getItem(this.ACCESS_TOKEN),
-    };
+    }
   }
   getAuthHeaders() {
     return {
       headers: {
         Authorization: localStorage.getItem(this.ACCESS_TOKEN),
-      }
+      },
     }
   }
 
   tokenExpired() {
-    const expDate = Number(localStorage.getItem(this.EXPIRATION));
+    const expDate = Number(localStorage.getItem(this.EXPIRATION))
     if (expDate > Date.now()) {
-      return false;
+      return false
     }
-    return true;
+    return true
   }
 
   storeAccessToken(token) {
-    localStorage.setItem(this.ACCESS_TOKEN, `Bearer ${token}`);
-    localStorage.setItem(this.EXPIRATION, this.getExpiration(token));
+    localStorage.setItem(this.ACCESS_TOKEN, `Bearer ${token}`)
+    localStorage.setItem(this.EXPIRATION, this.getExpiration(token))
   }
 
   getExpiration(token) {
-    let encodedPayload = token ? token.split(".")[1] : null;
+    let encodedPayload = token ? token.split(".")[1] : null
     if (encodedPayload) {
-      encodedPayload = encodedPayload.replace(/-/g, "+").replace(/_/g, "/");
-      const payload = JSON.parse(window.atob(encodedPayload));
-      return payload?.exp ? payload?.exp * 1000 : 0;
+      encodedPayload = encodedPayload.replace(/-/g, "+").replace(/_/g, "/")
+      const payload = JSON.parse(window.atob(encodedPayload))
+      return payload?.exp ? payload?.exp * 1000 : 0
     }
-    return 0;
+    return 0
   }
 
   handleError(errorCallBack) {
     return (error) => {
-      let message = "Could't connect to the server";
+      let message = "Could't connect to the server"
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
         if (error.response.status) {
-          message = error.response.data?.message || "The server replayed with error code";
+          message =
+            error.response.data?.message ||
+            "The server replayed with error code"
         }
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
+        console.log(error.response.data)
+        console.log(error.response.status)
+        console.log(error.response.headers)
       } else if (error.request) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        console.log(error.request);
-        message = error.request || "Oops... There is no response from the server";
-
+        console.log(error.request)
+        message =
+          error.request || "Oops... There is no response from the server"
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-        message = error.message || "Could't connect to the server";
+        console.log("Error", error.message)
+        message = error.message || "Could't connect to the server"
       }
-      errorCallBack(message);
-    };
+      errorCallBack(message)
+    }
   }
 }
-export default new Config();
+export default new Config()
