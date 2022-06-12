d0c5af6f4b938f94156e7d6e878eb884af8403e5
mengstabketemaw <mengstabrealk28@gmail.com>
2022-06-13 02:25:00 +0300

    change format

--- src/App.js
+++ src/App.js
@@ -1,44 +1,53 @@
 import { Routes, Route, Navigate } from "react-router"
-import Login from "./pages/Login";
-import CreateAccount from "./pages/CreateAccount";
-import Patient from "./pages/patient/Patient";
-import Dashboard from "./pages/patient/Dashboard";
-import Appointment from "./pages/patient/Appintment";
-import TherapyGroup from "./pages/patient/TherapyGroup";
-import Vdt from "./pages/patient/Vdt";
-import MedicalRecord from "./pages/patient/MedicalRecord";
-import ApplyForHelp from "./pages/patient/ApplyForHelp";
-import Profile from "./pages/patient/Profile";
-import Blog from "./pages/patient/Blog";
-import DoctorList from "./pages/patient/DoctorsList";
-import Room from "./pages/patient/Room";
-import Doctor from "./pages/doctor/Doctor";
-import Activity from "./pages/doctor/Activity";
-import Office from "./pages/doctor/Office";
-import HomeDoctor from "./pages/doctor/HomeDoctor";
-import DocBlog from "./pages/doctor/DocBlog";
-import Admin from "./pages/admin/Admin";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import Doctors from "./pages/admin/Doctors";
-import HelpApplications from "./pages/admin/HelpApplications";
-import Setting from "./pages/admin/Setting";
-import Feedback from "./pages/admin/Feedback";
-import Schedule from "./pages/doctor/Schedule";
-import PrivateElement from "./components/route/PrivateElement";
-import DocIndex from "./pages/doctor/DocIndex";
-import useToken from "./hooks/useToken";
-import DocTherapyGroup from "./pages/doctor/DocTherapyGroup";
-import DocVideoRoom from "./pages/doctor/DocVideoRoom";
+import Login from "./pages/Login"
+import CreateAccount from "./pages/CreateAccount"
+import Patient from "./pages/patient/Patient"
+import Dashboard from "./pages/patient/Dashboard"
+import Appointment from "./pages/patient/Appintment"
+import TherapyGroup from "./pages/patient/TherapyGroup"
+import Vdt from "./pages/patient/Vdt"
+import MedicalRecord from "./pages/patient/MedicalRecord"
+import ApplyForHelp from "./pages/patient/ApplyForHelp"
+import Profile from "./pages/patient/Profile"
+import Blog from "./pages/patient/Blog"
+import DoctorList from "./pages/patient/DoctorsList"
+import Room from "./pages/patient/Room"
+import Doctor from "./pages/doctor/Doctor"
+import Activity from "./pages/doctor/Activity"
+import Office from "./pages/doctor/Office"
+import HomeDoctor from "./pages/doctor/HomeDoctor"
+import DocBlog from "./pages/doctor/DocBlog"
+import Admin from "./pages/admin/Admin"
+import AdminDashboard from "./pages/admin/AdminDashboard"
+import Doctors from "./pages/admin/Doctors"
+import HelpApplications from "./pages/admin/HelpApplications"
+import Setting from "./pages/admin/Setting"
+import Feedback from "./pages/admin/Feedback"
+import Schedule from "./pages/doctor/Schedule"
+import PrivateElement from "./components/route/PrivateElement"
+import DocIndex from "./pages/doctor/DocIndex"
+import useToken from "./hooks/useToken"
+import DocTherapyGroup from "./pages/doctor/DocTherapyGroup"
+import DocVideoRoom from "./pages/doctor/DocVideoRoom"
 import Checkout from "./pages/pharmacy/Checkout"
 import Prescription from "./pages/patient/Prescription"
 
 function App() {
-  const { token } = useToken();
-  const loggedIn = Boolean(token?.username);
+  const { token } = useToken()
+  const loggedIn = Boolean(token?.username)
   return (
     <Routes>
       <Route path="/" element={<Navigate to="login" replace />} />
-      <Route path="login" element={loggedIn ? <Navigate to={`/user/${token.role.toLowerCase()}`} replace /> : <Login />} />
+      <Route
+        path="login"
+        element={
+          loggedIn ? (
+            <Navigate to={`/user/${token.role.toLowerCase()}`} replace />
+          ) : (
+            <Login />
+          )
+        }
+      />
       <Route path="create-account" element={<CreateAccount />} />
       <Route path="user" element={<PrivateElement />}>
         <Route path="patient" element={<Patient />}>
@@ -81,12 +90,19 @@ function App() {
         </Route>
       </Route>
       <Route path="pharmacy/:prescriptionId" element={<Checkout />} />
-      <Route path="*" element={<><h1>{noItem}</h1></>} />
+      <Route
+        path="*"
+        element={
+          <>
+            <h1>{noItem}</h1>
+          </>
+        }
+      />
     </Routes>
-  );
+  )
 }
 const noItem = `
 Sorry the item you are looking is not here, why don't you first find it in your HEAD! then we can try agine ;)
-`;
+`
 
-export default App;
+export default App
