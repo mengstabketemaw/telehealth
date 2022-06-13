91bb4de02fceeac797a009b2d3cc8dae6049e2e5
mengstabketemaw <mengstabrealk28@gmail.com>
2022-06-13 17:23:49 +0300

    disabled account can't access the system any longer

--- src/components/route/PrivateElement.js
+++ src/components/route/PrivateElement.js
@@ -1,5 +1,9 @@
-import React from "react"
-import { Navigate, Outlet, useLocation } from "react-router-dom"
+import { Box, Button, CircularProgress, Typography } from "@mui/material"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom"
+import Auth from "../../api/Auth"
+import Config from "../../api/Config"
 import useToken from "../../hooks/useToken"
 
 /**
@@ -7,17 +11,70 @@ import useToken from "../../hooks/useToken"
  * If authenticated, renders the passed element
  * If not authenticated, redirects the user to Login page.
  */
+const sx = {
+  height: "100vh",
+  width: "100%",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}
 const PrivateElement = ({ children }) => {
+  const [data, setData] = useState({ loading: false, disabled: false })
   let location = useLocation()
-  const {token} = useToken();
-  const role = token?.role;
-  if(Boolean(role)){
-    if(location.pathname.includes(role.toLowerCase()))
-        return <Outlet/>
-    else    
-        return <Navigate to={role.toLowerCase()}/>
+  const nav = useNavigate()
+  const { token, setToken } = useToken()
+  const role = token?.role
+  useEffect(() => {
+    ;(async function () {
+      if (!token?.userId) return
+      setData({ ...data, loading: true })
+      const {
+        data: {
+          user: { disabled },
+        },
+      } = await axios.get(Config.USER_URL + "/id/" + token.userId)
+      console.log(disabled)
+      setData({ loading: false, disabled })
+    })()
+  }, [token?.userId])
+
+  if (data.loading) {
+    return (
+      <Box sx={sx}>
+        <CircularProgress />
+        <Typography>Loading</Typography>
+      </Box>
+    )
   }
-  return <Navigate to="/login" state={{from:location}}/>
+
+  if (data.disabled) {
+    return (
+      <Box sx={{ ...sx, flexDirection: "column" }}>
+        <Typography variant="h4" color="error">
+          Your Account is Disabled!
+        </Typography>
+        <Typography>
+          You cannot use the system functionality until your account is enabled.
+        </Typography>
+        <br />
+        <Button
+          onClick={() => {
+            nav("/login")
+            let auth = new Auth(token, setToken)
+            auth.logoutUser()
+          }}
+        >
+          Logout
+        </Button>
+      </Box>
+    )
+  }
+
+  if (Boolean(role)) {
+    if (location.pathname.includes(role.toLowerCase())) return <Outlet />
+    else return <Navigate to={role.toLowerCase()} />
+  }
+  return <Navigate to="/login" state={{ from: location }} />
 }
 
 export default PrivateElement
